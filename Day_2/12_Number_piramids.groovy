dd3d849ead04e22b299087b98b960e9c925c8429
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-10-04 11:09:31 +0100

    First attempt - probably the lazy way I will look at simplifying the code soon

---
+++ Day_2/12_Number_piramids.groovy
@@ -0,0 +1,38 @@
+// Task:
+// Write a program that output a number piramid like the one below.
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+
+int b
+for (b = 0; b < 1; b++) {
+   print "1"
+}
+println ""
+for (b = 0; b < 2; b++) {
+   print "2"
+}
+println ""
+for (b = 0; b < 3; b++) {
+   print "3"
+}
+println ""
+for (b = 0; b < 4; b++) {
+   print "4"
+}
+println ""
+for (b = 0; b <5; b++) {
+   print "5"
+}
+println ""
+for (b = 0; b <6; b++) {
+   print "6"
+}
+println ""
+for (b = 0; b <7; b++) {
+   print "7"
+}
