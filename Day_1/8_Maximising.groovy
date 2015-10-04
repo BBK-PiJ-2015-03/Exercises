4300d590bde5a80f8a031d9f74089dfff290de1f
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-10-04 08:16:12 +0100

    First attempt at question 8) Maximising

---
+++ Day_1/8_Maximising.groovy
@@ -0,0 +1,20 @@
+// Question:
+// Write a program that read a (arbitrarily long) sequence of positive numbers.
+// The sequence is ended when the users enters “-1”. At that point, the program
+// must output the highest number in the sequence.
+
+int max = 0, i
+String str
+boolean finished = false
+prinln "Enter a sequence of numbers (end with -1)"
+while (!finished) {
+    print "> "
+    str = System.console().readLine()
+    i = Integer.parseInt(str)
+    if (i == -1) {
+        print max
+        finished = true
+    } else if (i > max) {
+        max = i
+    }
+}
