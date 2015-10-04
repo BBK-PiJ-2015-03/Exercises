dcf6f6bd7656c744adbc691f86827d18c17aea0b
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-10-04 06:55:46 +0100

    First attempt at Multiplication without unsing '*' - have not executed to test yet

---
+++ Day_1/5_Multiplication.groovy
@@ -0,0 +1,24 @@
+// Question:
+// Write a program that requests two numbers from the user and then outputs its
+// product. You cannot use the “*” operator.
+
+boolean finished = false
+int num1, num2, total, count = 0
+String str
+println "Multiplication Calculator"
+println "Enter a number"
+print "> "
+str = System.console().readLine()
+num1 = Integer.parseInt(str)
+println "Enter another number"
+print "> "
+str = System.console().readLine()
+num2 = Integer.parseInt(str)
+while (!finished) {
+    count++;
+    total = (num1 + num1) + total
+    if (count == num2) {
+        finished = true
+    }
+}
+println total
