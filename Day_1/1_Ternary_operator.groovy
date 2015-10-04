d7cfdc24ad1fe2469695721df53b9410763af98f
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-10-04 05:10:04 +0100

    Corrected grammar mistake on line 2 - you should be your

--- Day_1/1_Ternary_operator.groovy
+++ Day_1/1_Ternary_operator.groovy
@@ -1,5 +1,5 @@
-// This program asks user for year of birth and deducts that from the 'current year' and output
-// 'It seems you are an adult.' If you age is 18 or higher
+// This program asks user for year of birth and deducts that from the 'current year' and outputs
+// 'It seems you are an adult.' If your age is 18 or higher
 // otherwise it will output 'It seems you are not an adult.' 
 int currentYear = 2012
 print "When were you born (year)?"
