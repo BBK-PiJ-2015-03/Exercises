79ab5fa1afa44f6c4b810d72d1248c897c079d67
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-10-04 07:54:09 +0100

    The program was missing an additional if statement, it would not output a result if the first number was 2, second 1, third 3. I have added the following to fix this: } else if (num3 > num1 && num1 > num2) { println num2 + ", " + num1 + ", " + num3 }

--- Day_1/7_Naive_sorting.groovy
+++ Day_1/7_Naive_sorting.groovy
@@ -24,4 +24,6 @@ if (num1 > num2 && num2 > num3) {
     println num3 + ", " + num1 + ", " + num2
 } else if (num1 > num3 && num3 > num2) {
     println num2 + ", " + num3 + ", " + num1
+} else if (num3 > num1 && num1 > num2) {
+    println num2 + ", " + num1 + ", " + num3
 }
