d58e558429d162e85ce53ea4eb7b59b6a4ec643a
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-10-11 03:30:57 +0100

    Added a recursive version of the code

--- Day_5/02.1.a_Factorial.groovy
+++ Day_5/02.1.a_Factorial.groovy
@@ -3,10 +3,24 @@ Write a small program with a method that calculates the factorial of an integer 
 Is it easy to do this both iteratively and recursively? Try both ways and see which is more natural for you. If
 one takes too long, try the other way.*/
 
+//Iterative version
 int numFactorial(int n) {
     for (int i = n-1; i != 0; i = i-1) {
         n = (n * i)
     }
     return n
 }
+
+//Recursive version (did have help from the 'basic recursion' document)
+int numFactorial(int n) {
+    if (n == 1) {
+        return n;
+    } else {
+    int i = n-1
+    n = n*numFactorial(i)
+    return n
+    }
+}
+println numFactorial(5)
+
 
