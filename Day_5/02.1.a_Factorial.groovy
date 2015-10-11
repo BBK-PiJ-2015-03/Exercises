c65d6a8296baa0e428d46a97df0b4560236fefa3
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-10-11 02:49:44 +0100

    Calculates square of number rather than factorial This is my first attempt, I will look over code to correct it

---
+++ Day_5/02.1.a_Factorial.groovy
@@ -0,0 +1,14 @@
+/*TASK
+Write a small program with a method that calculates the factorial of an integer number as seen in the notes.
+Is it easy to do this both iteratively and recursively? Try both ways and see which is more natural for you. If
+one takes too long, try the other way.*/
+
+int numFactorial(int n) {
+    int c, p
+    for (int i = 0; i != n; i++) {
+        p = n * i
+        c = p + n
+    }
+    return c
+}
+
