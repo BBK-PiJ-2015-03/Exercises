9999c379a0e6b4c7d9d8b0c14d74c9a6614b483f
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-10-12 13:53:28 +0100

    Adjusted the code as I believe my first attempt was slightly incorrect, The code now executes exactly as expected.

--- Day_5/01.b_Recursive_code_line_by_line.groovy
+++ Day_5/01.b_Recursive_code_line_by_line.groovy
@@ -1,21 +1,24 @@
 /*TASK
 Do you see anything wrong with the following code? How would you fix it?
-String buggyMethod(int n) {
+String buggyMethod(int n) {     // Indicates that we must return a string
 	if (n == 0) {
 		return "";
 	}
-	return n + " " + buggyMethod(n - 2);
+	return n + " " + buggyMethod(n - 2);    // We need to return a string n is an Integer
 } */
 
-String buggyMethod(int c) {
-    if (c == 0) {
-    return "";
-    }
-    String n = c + " " + buggyMethod(c - 2); //n is now a string
-    return n
+String buggyMethod(int n) {
+if (n == 0) {
+return "";
 }
+String p = n + " " + buggyMethod(n - 2); // This is now a string
+return p // Returns the string
+}
+
 println buggyMethod(10)
 
 /* Result of code is:
-01	10 8 6 4 2
+10 8 6 4 2
+The code counts down from the integer put inside the parameters in two's untill it reaches
+0, at this point it adds the result to a string with a space between out each count down.
 */
