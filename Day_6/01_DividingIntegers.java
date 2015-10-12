c60d603387e944717d3771782ae4a1c56e9595aa
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-10-12 23:01:43 +0100

    fixed the calculator! all works well problem was that I did not initialise class variable, forgive me if my terminology is wrong.

--- Day_6/01_DividingIntegers.java
+++ Day_6/01_DividingIntegers.java
@@ -1,35 +1,57 @@
 /* TASK
-Create a Java class called Calculator. The class should implement the following methods, each of them printing
-the result on the screen.
-• add(int, int)
-• subtract(int, int)
-• multiply(int, int)
-• divide(int, int)
-• modulus(int, int)
-Note that you will will need to cast the parameters into double to perform exact division.
-Write a small Groovy or Java Decaf program that uses all the methods of Calculator.
-*/
-class Calculator {
-            double x
-            double y
-            int t
-            int add(int x, int y) {
-                this.t = x+y
-                return t
-            }
-            int subtract(int x, int y) {
-                return x-y
-            }
-            int multiply(int x, int y) {
-                return x*y
-            }
-            double divide(int x, int y) {
-                this.x = (double) x
-                this.y = (double) y
-                return this.x/this.y
-            }
-            int modulus(int x, int y) {
-                return x%y
-            }
+ Create a Java class called Calculator. The class should implement the following methods, each of them printing
+ the result on the screen.
+ • add(int, int)
+ • subtract(int, int)
+ • multiply(int, int)
+ • divide(int, int)
+ • modulus(int, int)
+ Note that you will will need to cast the parameters into double to perform exact division.
+ Write a small Groovy or Java Decaf program that uses all the methods of Calculator.
+ */
+public class Calculator {
+    private double x
+    private double y
+    public void add(int x, int y) {
+        println x+y
+    }
+    public void subtract(int x, int y) {
+        println x-y
+    }
+    public void multiply(int x, int y) {
+        println x*y
+    }
+    public void divide(int x, int y) {
+        this.x = (double) x
+        this.y = (double) y
+        println this.x/this.y
+    }
+    public void modulus(int x, int y) {
+        println x%y
+    }
 }
-System.out.println("Testing Addition: 14 + 16 = " + Calculator.add(14,16)) 
+
+Calculator calc = new Calculator()
+
+//Testing Addition
+System.out.print("16 + 14 = ")
+calc.add(16,14)
+
+//Testing Subtration
+System.out.print("6 - 3 = ")
+calc.subtract(6,3)
+
+//Testing Multiplication
+System.out.print("11 x 5 = ")
+calc.multiply(11,5)
+
+//Testing Division
+System.out.print("25 / 4 = ")
+calc.divide(25,4)
+
+//Testing Modulus
+System.out.print("14 % 6 = ")
+calc.modulus(14,6)
+
+
+
