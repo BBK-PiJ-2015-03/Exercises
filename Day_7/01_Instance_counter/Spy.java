9c73e1b99cfa77115e6adb076a9a425c176db826
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-10-19 01:32:55 +0100

    A start on this excersize, spyCount is working as per example

---
+++ Day_7/01_Instance_counter/Spy.java
@@ -0,0 +1,13 @@
+public class Spy {
+	private static int spyCount = 0;
+
+	public Spy() {
+		spyCount++;
+	}
+
+	public static int getNumberOfSpies() {
+		return spyCount;
+
+	}
+}
+
---
+++ Day_7/01_Instance_counter/main.java
@@ -0,0 +1,6 @@
+public class main {
+	public static void main(String[] args) {
+		Spy jimmy = new Spy();
+		System.out.println(jimmy.getNumberOfSpies());
+	}
+}
