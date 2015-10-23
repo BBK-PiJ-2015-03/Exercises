272fa63ac3628a56e0d25defc2abe17ce4f109e2
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-10-23 06:30:42 +0100

    have spent a few hours trying to sort this out no luck first element always get shifted to last when trying to delete.

--- Day_7/06_Circular_lists/LinkedList.java
+++ Day_7/06_Circular_lists/LinkedList.java
@@ -40,30 +40,77 @@ public class LinkedList {
 
 
 	public boolean del(int data){
-		if (size == 0) {
+		/**
+		*Scenario 1: Node at start
+		*Scenario 2: Node in middle
+		*Scenario 3: Node at end
+		*Scenario 4: No nodes in list
+		*Scenario 5: Node is the only one in list
+		*/
+
+		if (head == null) {
 			System.out.println("List is empty.");
 			return false;
-		} else {
-			Node temp = new Node();
-			temp = head;
-			while (temp.data != data){
-				if (temp.next == head){
-					return false;
-				}
-
-				temp = temp.next;
-			}
-			if (head == temp) {
-				head = temp.next;
-				temp.next = head;
-				return true;
-			} else {
-				previousNode(data).next = temp.next;
-				return true;
-			}
-			// under construction
+			//Scenario 4
 		}
+		
+		if (head.data == data){
+			System.out.print("DEBUGTEST2");
+			head = head.next;
+			return true;
+		} 
+			//Scenario 5 & Scenario 1
+		
+		Node temp = new Node();
+		temp = head;
+		while (temp.data != data) {
+			if (temp.next != head) {
+								System.out.print("DEBUGTEST4");
+				temp = temp.next;
+			} else {
+								System.out.print("DEBUGTEST5");
+
+			return false;
+			}
+		}
+		previousNode(data).next = temp.next;
+						System.out.print("DEBUGTEST6");
+
+		return true;
+		//Scenario 2
 	}
+
+		/**
+		* Old method that was not deleting first element of list
+		*
+		* if (head == null) {
+		*	System.out.println("List is empty.");
+		*	return false;
+		* } else {
+		*	Node temp = new Node();
+		*	temp = head;
+		*	if (head.data == data){
+		*		head = head.next;
+		*		return true;
+		*	}
+		*	while (temp.data != data){
+		*		if (temp.next == head){
+		*			return false;
+		*		}
+		*
+		*		temp = temp.next;
+		*	}
+		*	if (head == temp) {
+		*		head = temp.next;
+		*		temp.next = head;
+		*		return true;
+		*	} else {
+		*		previousNode(data).next = temp.next;
+		*		return true;
+		*	}
+		*	// under construction
+		* }
+		*/
 
 
 	public Node previousNode(int data){
