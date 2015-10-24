2f998c544274f302e4c93c6f0b3b07ef9200f967
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-10-24 21:47:15 +0100

    Cannot compile example code so I cannot test it

---
+++ Day_8/Ex_Pg5/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+	public static void main(String[] args) {
+	
+		//Person son = new AdultPerson();
+		// move in front of mother
+		//son.move(10);
+		// give the message
+		//son.say("I love you, Mum.");
+
+	}
+
+
+}
