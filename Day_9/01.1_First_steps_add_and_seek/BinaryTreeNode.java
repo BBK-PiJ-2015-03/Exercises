37b0feb47893999836f5e3df5801ce882fd289fd
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-10-26 07:01:22 +0000

    Working Binary Tree - getMin() & getMax() methods implemented

---
+++ Day_9/01.1_First_steps_add_and_seek/BinaryTreeNode.java
@@ -0,0 +1,51 @@
+public class BinaryTreeNode {
+	int value = 0;
+	BinaryTreeNode right;
+	BinaryTreeNode left;
+	
+	public BinaryTreeNode(int value){
+		this.value = value;
+		right = null;
+		left = null;
+	}
+	
+	public BinaryTreeNode() {
+		//empty constructor
+	}
+
+	public void add(int newNumber){
+		if (newNumber > this.value){
+			if (right == null){
+				right = new BinaryTreeNode(newNumber);
+				System.out.println(newNumber + " has been added to the tree");
+			} else {
+				right.add(newNumber);
+			}
+		} else {
+			if (left == null){
+				left = new BinaryTreeNode(newNumber);
+				System.out.println(newNumber + " has been added to the tree");
+			} else {
+				left.add(newNumber);
+			}
+		}
+	}
+	
+	
+	public int getMax(){
+		BinaryTreeNode holder = right;
+		while (holder.right != null) {
+			holder = holder.right;
+		}
+		return holder.value;
+	}
+
+	public int getMin(){
+		BinaryTreeNode holder = right;
+		while (holder.left != null) {
+			holder = holder.left;
+		}
+		return holder.value;
+	}
+	
+}
